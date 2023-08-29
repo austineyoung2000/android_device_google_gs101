684d853ee4cca720ca4e25ac13034595f6ca9a68
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 13:22:44 -0500

    gs101: Remove elf files in copy files check
    New extract files uses packages instead of copy files
    
    Change-Id: I337c07d13bd350958d4808e1693cc6e03753a96c

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,13 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_DUP_RULES := true
 
-TARGET_FORCE_PREBUILT_KERNEL := true
-TARGET_KERNEL_CONFIG := cloudripper_gki_defconfig
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_KERNEL_SOURCE := kernel/google/raviole/raven
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
+TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
