f2fba9262ca8a29f461e907f7391bb2c418e88ee
jrcable1 <jrcable1@gmail.com>
2024-08-27 06:33:47 -0400

    gs101: Add build broken duplication flag
    -eliminates unnecessary duplicates in vendor/gms

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,6 +5,7 @@
 #
 
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_DUP_RULES := true
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
